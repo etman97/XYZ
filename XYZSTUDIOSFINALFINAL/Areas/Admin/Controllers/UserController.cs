3ac8f7a6f91dd9e50d6cc017b22f8de6ea77a5f8
etman <mahmoud.etman@ieee-zsb.org>
2020-10-14 00:12:38 +0200

    add

---
+++ XYZSTUDIOSFINALFINAL/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using XYZSTUDIOSFINALFINAL.Models.Data;
+using XYZSTUDIOSFINALFINAL.Models.ViewModels.Account;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Helpers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XYZSTUDIOSFINALFINAL.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            // Declare list of PageVM
+            List<UserVM> UserList;
+
+            using (Db db = new Db())
+            {
+                // Init the list
+                UserList = db.Users.ToArray().OrderBy(x => x.Id).Select(x => new UserVM(x)).ToList();
+            }
+
+            // Return view with list
+            return View(UserList);
+        }
+    }
+}
